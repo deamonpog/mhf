b456bb233980c02b946a45a671a6cdce42249198
deamonpog <pog666@gmail.com>
2021-08-27 20:38:07 -0400

    Turning achieved included with movement. Need more work.

---
+++ UnityProject/MainMHF/Assets/Scripts/Sc_Unit.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sc_Unit : MonoBehaviour
+{
+    public Collider CollisionBoundingVolume;
+
+    public GameObject _Planet;
+    
+    float _PlanetRadius;
+
+    float linearSpeed = 0.0f;
+
+    public float mass = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _PlanetRadius = _Planet.transform.lossyScale.x / 10.0f;
+        Vector3 p_to_this = (transform.position - _Planet.transform.position).normalized;
+        transform.position = _PlanetRadius * p_to_this;
+        //transform.rotation = Quaternion.Euler( Vector3.Cross(transform.up, transform.position.normalized) );
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //print(transform.up.normalized);
+
+        Quaternion finalRotation = transform.rotation;
+
+        Vector3 p_to_this = (transform.position - _Planet.transform.position).normalized;
+
+        float forwardForceRatio = Input.GetAxis("Vertical");
+        
+        float accelerationForce = 10f * forwardForceRatio;
+
+        float acceleration = accelerationForce / mass;
+
+        linearSpeed += acceleration * Time.deltaTime;
+
+        transform.position += transform.forward.normalized * linearSpeed * Time.deltaTime;
+
+        Quaternion q = Quaternion.FromToRotation(Vector3.up, p_to_this);
+
+        finalRotation = q;
+
+        float turnRightForceRatio = Input.GetAxis("Horizontal");
+
+        if (turnRightForceRatio > 0)
+        {
+            Quaternion qturn = Quaternion.AngleAxis(30f , Vector3.up);
+            print("turning");
+            finalRotation = q * qturn;
+        }
+
+        transform.rotation = finalRotation;
+
+    }
+}
